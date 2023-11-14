7fc9aaf664dee8f2eef503a6d5c3dfcfb8e7bb96
paesol <102522046+faisalekasyahputra@users.noreply.github.com>
2023-11-14 11:47:33 +0700

    Update bangunan.php

--- application/views/bo/bangunan.php
+++ application/views/bo/bangunan.php
@@ -1,9 +1,5 @@
 <div class="main-panel">
         <div class="content-wrapper">
-        
-            
-            
-            
             <div class="col-lg-12 grid-margin stretch-card">
               <div class="card">
                 <div class="card-body">
