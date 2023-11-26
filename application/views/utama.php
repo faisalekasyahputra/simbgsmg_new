7d18f26b6bdf5047580f827328ce7159d417c6a5
paesol <102522046+faisalekasyahputra@users.noreply.github.com>
2023-11-27 02:19:57 +0700

    Update utama.php

--- application/views/utama.php
+++ application/views/utama.php
@@ -1,44 +1,35 @@
-<section id="section-1" class="hero-section" style="background-image: url(<?=base_url('assets/img/bg-depan.jpg')?>); background-size: cover;">
-        <div class="container-fluid d-flex" style="background-image: url('assets/img/bgover.png'); background-size: cover; min-height: 100vh; margin-top:-90px;">
-            <div class="container m-auto">
-                <div class="row justify-content-end">
-                    <div class="col-6 d-flex flex-column align-items-end">
-                        <h1 class="display-4 text-end fw-bolder judul"><i class="fa fa-building p-1 me-2" aria-hidden="true"></i> Sistem Informasi Manajemen
-                            Bina Penataan Bangunan</h1>
-                        <p class="lead fs-6 fw-bold">Dinas PUPR Pemerintah Kota Semarang.</p>
-                        <a class="btn btn-primary btn-lg" href="<?=base_url('Welcome/peta')?>"><i class="fa fa-database me-2" aria-hidden="true"></i> Lihat Data</a>
-                    </div>
+<section id="section-1" class="hero-section" style="background-image: url(<?= base_url('assets/img/bg-depan.jpg') ?>); background-size: cover;">
+    <div class="container-fluid d-flex" style="background-image: url('assets/img/bgover.png'); background-size: cover; min-height: 100vh; margin-top:-90px;">
+        <div class="container m-auto">
+            <div class="row justify-content-end">
+                <div class="col-6 d-flex flex-column align-items-end">
+                    <h1 class="display-4 text-end fw-bolder judul"><i class="fa fa-building p-1 me-2" aria-hidden="true"></i> Sistem Informasi Manajemen
+                        Bina Penataan Bangunan</h1>
+                    <p class="lead fs-6 fw-bold">Dinas PUPR Pemerintah Kota Semarang.</p>
+                    <a class="btn btn-primary btn-lg" href="<?= base_url('Welcome/peta') ?>"><i class="fa fa-database me-2" aria-hidden="true"></i> Lihat Data</a>
                 </div>
-
             </div>
+
         </div>
+    </div>
 
-    </section>
+</section>
 
-    <!-- Section 2 -->
-    <section id="section-2">
-        <div class="container-fluid hero-bg py-5" style="background-image: url('<?=base_url('/assets/img/halftone.svg')?>');">
+<!-- Section 2 -->
+<section id="section-2">
+
+    <div class="container-fluid py-5 " style="background-image: url('<?= base_url('/assets/img/halftone.svg') ?>'); background-size:cover;">
         <div class="container mb-5">
             <div class="row align-items-center">
-                
+
                 <div class="col-2">
-                    <h1 class="display-3 text-center judul position-absolute" style="z-index: 2;">VISI</h1>
+                    <h1 class="display-3 text-center judul position-absolute ms-5 m-auto" style="z-index: 2;">VISI</h1>
                     <img class="square " src="assets/img/square.png" alt="" width="500">
                 </div>
                 <div class="col-8">
-                    <ul class="list-group list-group-flush align-items-center ">
-                        <li class="list-group-item border border-0">Tell your visitors about your company and what you
-                            do.</li>
-                        <li class="list-group-item border border-0">Tell your visitors about your company and what you
-                            do.</li>
-                        <li class="list-group-item border border-0">Tell your visitors about your company and what you
-                            do.</li>
-                        <li class="list-group-item border border-0">Tell your visitors about your company and what you
-                            do.</li>
-                        <li class="list-group-item border border-0">Tell your visitors about your company and what you
-                            do.</li>
-                        <li class="list-group-item border border-0">Tell your visitors about your company and what you
-                            do.</li>
+                    <ul class="list-group list-group-flush glass rounded p-3">
+                        <li class="list-group-item border border-0 text-center bg-transparent">Menjadi sistem informasi manajemen bangunan gedung terdepan yang memberikan kemudahan, percepatan, dan peningkatan pelayanan atas perizinan PGB dan SLF.</li>
+
                     </ul>
 
                 </div>
@@ -48,29 +39,46 @@
             <div class="row justify-content-end align-items-center">
 
                 <div class="col-8">
-                    <ul class="list-group list-group-flush align-items-center border-none">
-                        <li class="list-group-item border border-0">Tell your visitors about your company and what you
-                            do.</li>
-                        <li class="list-group-item border border-0">Tell your visitors about your company and what you
-                            do.</li>
-                        <li class="list-group-item border border-0">Tell your visitors about your company and what you
-                            do.</li>
-                        <li class="list-group-item border border-0">Tell your visitors about your company and what you
-                            do.</li>
-                        <li class="list-group-item border border-0">Tell your visitors about your company and what you
-                            do.</li>
-                        <li class="list-group-item border border-0">Tell your visitors about your company and what you
-                            do.</li>
+                    <ul class="list-group list-group-flush align-items-start glass rounded p-3">
+                        <li class="list-group-item border border-0 bg-transparent">- Memberikan kemudahan dan aksesibilitas dalam pengurusan perizinan bangunan gedung.</li>
+                        <li class="list-group-item border border-0 bg-transparent">- Meningkatkan efektivitas dan efisiensi dalam penyelenggaraan bangunan gedung.</li>
+                        <li class="list-group-item border border-0 bg-transparent">- Menjamin keandalan teknis bangunan gedung.</li>
+                        <li class="list-group-item border border-0 bg-transparent">- Meningkatkan koordinasi antar perangkat daerah terkait secara elektronik atau online, melalui Online Single Submission (OSS).
+                        </li>
+
                     </ul>
 
                 </div>
                 <div class="col-2">
-                    <h1 class="display-3 text-center judul position-absolute" style="z-index: 2;">MISI</h1>
-                    <img class="square " src="<?=base_url('assets/img/square.png')?>" alt="" width="500">
+                    <h1 class="display-3 text-center judul position-absolute ms-5 m-auto" style="z-index: 2;">MISI</h1>
+                    <img class="square " src="<?= base_url('assets/img/square.png') ?>" alt="" width="500">
                 </div>
             </div>
         </div>
     </div>
-    </section>
+</section>
+<section id="footer">
+    <footer>
+        <div class="container-fluid bg-dark text-white d-flex">
+            <div class="container p-3 my-5 m-auto">
+                <div class="row " >
+                    <div class="col-md-1">
+                        <img src="<?=base_url('assets/img/logo_smg.png')?>" alt="" width="100">
+                    </div>
+                    <div class="col">
+                        <p class="m-0">
+                        Pemerintah Kota Semarang 
+                        </p>
+                        <p style="font-size: xx-small;">
+                        Jl. Pemuda No. 148 Semarang Kode Pos: 50132 <br>Telp: (024) 3513366 – 3515871 Fax: (024) 3542522
+                        </p>
+                    </div>
+                    <div class="col-md-2" >
+                    <iframe class="card" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3960.229092684039!2d110.40796829678958!3d-6.9822702!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e708ba87e15e37b%3A0xb6685e7f890af6d9!2sBalai%20Kota%20Semarang!5e0!3m2!1sid!2sid!4v1699961249215!5m2!1sid!2sid" width="100%" height="100%" style="border:0; ;"  allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
 
-   
+</section>
